2db8e2c86148de44266c30b6a6ff2378731b8514
Vinay Kumar Patel <vinayptl47@gmail.com>
2026-07-18 22:49:58 +0530

    added deployed url for fontend and backend

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ app.add_middleware(
         CORSMiddleware,
         allow_origins=[
             "http://localhost:5173",
-            "http://127.0.0.1:5173",
+            "https://newspulse-ai-two.vercel.app",
         ],
         allow_credentials=True,
         allow_methods=["*"],
